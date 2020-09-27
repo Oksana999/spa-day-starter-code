2df5b43c9c152de365f61ec446f8374c3c5b6e26
Oksana999 <oksanam999@gmail.com>
2020-09-27 17:16:26 -0500

    Studio: Spa Day!

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -1,14 +1,20 @@
 package org.launchcode.spaday.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 
 @Controller
 public class SpaDayController {
+
+   // private static String name;
 
     public boolean checkSkinType(String skinType, String facialType) {
         if (skinType.equals("oily")) {
@@ -46,7 +52,7 @@ public class SpaDayController {
     @GetMapping(value="")
     @ResponseBody
     public String customerForm () {
-        String html = "<form method = 'post'>" +
+        String html = "<form method = 'post' action=''>" +
                 "Name: <br>" +
                 "<input type = 'text' name = 'name'>" +
                 "<br>Skin type: <br>" +
@@ -57,19 +63,20 @@ public class SpaDayController {
                 "<option value = 'dry'>Dry</option>" +
                 "</select><br>" +
                 "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
+                "<select name='manipedi' id='manipedi' multiple>" +
                 "<option value = 'manicure'>Manicure</option>" +
                 "<option value = 'pedicure'>Pedicure</option>" +
                 "</select><br>" +
                 "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
+                "</form>" +
+        "<p>Hold down the Ctrl (windows) or Command (Mac) button to select multiple options.</p>";
         return html;
     }
 
     @PostMapping(value="")
-    public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
+    public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String[] manipedi, HttpSession session) {
 
-        ArrayList<String> facials = new ArrayList<String>();
+        ArrayList<String> facials = new ArrayList<>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
         facials.add("Rejuvenating");
@@ -82,6 +89,40 @@ public class SpaDayController {
             }
         }
 
+        session.setAttribute("name", name);
+        session.setAttribute("skintype", skintype);
+        session.setAttribute("manipedi", manipedi);
+
+        return "redirect:menu";
+    }
+
+    @GetMapping(value="/menu")
+    public String renderMenu(HttpSession session, Model model){
+        model.addAttribute("name", session.getAttribute("name"));
+        model.addAttribute("skintype", session.getAttribute("skintype"));
+        model.addAttribute("manipedi", session.getAttribute("manipedi"));
+
+        model.addAttribute("appropriateFacials", session.getAttribute("appropriateFacials"));
         return "menu";
     }
+
+//    @GetMapping("/p1")
+//    public String p1(Model model){
+//        model.addAttribute("msg", "MSG_1");
+//        return "p1";
+//    }
+//
+//    @PostMapping("/p1")
+//    public String p1(@RequestParam String name, HttpSession session){
+//        session.setAttribute("name", name);
+//        return "redirect:p2";
+//    }
+//
+//    @GetMapping("/p2")
+//    public ModelAndView p2(HttpSession session){
+//        ModelAndView mv = new ModelAndView();
+//        mv.setViewName("p2");
+//        mv.addObject("name", session.getAttribute("name"));
+//        return mv;
+//    }
 }
