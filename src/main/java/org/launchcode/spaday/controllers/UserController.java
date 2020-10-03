233cb6f6798bdf7642756fa016b509a1f7b5d832
Oksana999 <oksanam999@gmail.com>
2020-10-03 01:49:46 -0500

    13.6. Studio: Spa User Validation

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,32 +3,35 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/add")
-    public String displayAddUserForm() {
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+       model.addAttribute("title", "Add User");
+        model.addAttribute(new User());
         return "user/add";
     }
 
-    @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-           return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        }
+    @PostMapping("add")
+    public String processAddUserForm(@ModelAttribute  @Valid User newUser , Errors errors, Model model) {
+
+      User user = new User();
+      user.setPassword(newUser.getPassword());
+      user.setVerifyPassword(newUser.getVerifyPassword());
+       if (errors.hasErrors()) {
+          model.addAttribute("title", "Add User");
+          return "user/add";
+       }
+
+          return "user/index";
 
     }
-
-
 }
+
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,25 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
 public class User {
+    @NotBlank(message = "Please, enter user name")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid email. Try again.")
     private String email;
+
+    @NotBlank(message = "Please, enter your password")
+    @Size(min=6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
@@ -38,5 +54,36 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword(this.password, this.verifyPassword);
+
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword(this.password, this.verifyPassword);
+
+    }
+
+    private void checkPassword(String password, String verifyPassword){
+        if(password != null && (verifyPassword != null) && (!password.equals(verifyPassword))){
+            setVerifyPassword(null);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPassword());
     }
 }
